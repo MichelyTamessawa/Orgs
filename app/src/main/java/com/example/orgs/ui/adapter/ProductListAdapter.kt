054f758ffd1d2e2ed4056e9faf5b6c5f8c4b3139
MichelyTamessawa <michelytamessawa@gmail.com>
2023-05-30 17:50:42 -0300

    Initial commit

---
+++ app/src/main/java/com/example/orgs/ui/adapter/ProductListAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.orgs.ui.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.orgs.R
+import com.example.orgs.model.Product
+
+class ProductListAdapter(
+    val context: Context,
+    val productList: List<Product>
+) : RecyclerView.Adapter<ProductListAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(product: Product) {
+            val name = itemView.findViewById<TextView>(R.id.product_item_name)
+            val description = itemView.findViewById<TextView>(R.id.product_item_description)
+            val value = itemView.findViewById<TextView>(R.id.product_item_value)
+            name.text = product.name
+            description.text = product.description
+            value.text = product.value.toPlainString()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.product_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return productList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val product = productList[position]
+        holder.bind(product)
+    }
+}
